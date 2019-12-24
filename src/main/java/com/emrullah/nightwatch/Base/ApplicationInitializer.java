c65658bf289982b4b2cddc6e6ab72fd21c1d5f5e
Sangaibisi <emrullahyildirim@windowslive.com>
2019-12-24 07:09:39 +0300

    FXML and icon has edited.

--- src/main/java/com/emrullah/nightwatch/Base/ApplicationInitializer.java
+++ src/main/java/com/emrullah/nightwatch/Base/ApplicationInitializer.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,6 +23,7 @@ public class ApplicationInitializer extends Application {
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.centerOnScreen();
+        primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("crow.ico")));
 
         primaryStage.show();
     }
