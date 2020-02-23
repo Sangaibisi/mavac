f9492077ded2e9b0244b7f7c7b5e74413546abc5
Sangaibisi <emrullahyildirim@windowslive.com>
2020-02-23 18:41:14 +0300

    Module pojos updates

--- src/main/java/com/emrullah/mavac/Model/Module.java
+++ src/main/java/com/emrullah/mavac/Model/Module.java
@@ -1,6 +1,7 @@
 package com.emrullah.mavac.Model;
 
 import com.emrullah.mavac.Utility.GeneralEnums;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 
@@ -8,10 +9,15 @@ public class Module {
 
     private File file;
     private GeneralEnums.PriorityModules modulePriority;
+    private long moduleSizeBeforeRun;
+    private long moduleSizeAfterRun;
+
+    public Module(){}
 
     public Module(File file) {
         this.file = file;
         this.modulePriority = null;
+        this.moduleSizeBeforeRun = 0;
     }
 
     public Module(File file, GeneralEnums.PriorityModules modulePriority) {
@@ -21,6 +27,42 @@ public class Module {
 
     public File getFile() {
         return file;
+    }
+
+    public void setModuleSize(long moduleSizeBeforeRun) {
+        this.moduleSizeBeforeRun = moduleSizeBeforeRun;
+    }
+
+    public long getModuleSize() {
+        return moduleSizeBeforeRun;
+    }
+
+    public long getModuleSizeBeforeRun() {
+        return moduleSizeBeforeRun;
+    }
+
+    public void setModuleSizeBeforeRun(long moduleSizeBeforeRun) {
+        this.moduleSizeBeforeRun = moduleSizeBeforeRun;
+    }
+
+    public long getModuleSizeAfterRun() {
+        return moduleSizeAfterRun;
+    }
+
+    public void setModuleSizeAfterRun(long moduleSizeAfterRun) {
+        this.moduleSizeAfterRun = moduleSizeAfterRun;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public boolean isSizeChanged() {
+        return moduleSizeBeforeRun == moduleSizeAfterRun ? true : false;
+    }
+
+    public void setModulePriority(GeneralEnums.PriorityModules modulePriority) {
+        this.modulePriority = modulePriority;
     }
 
     public GeneralEnums.PriorityModules getModulePriority() {
--- src/main/java/com/emrullah/mavac/Model/Project.java
+++ src/main/java/com/emrullah/mavac/Model/Project.java
@@ -1,18 +1,21 @@
 package com.emrullah.mavac.Model;
 
-import com.emrullah.mavac.Utility.GeneralEnums;
-
 import java.io.File;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Project extends Module {
 
-    private List<Module> moduleList = new ArrayList<Module>();
+    private List<Module> moduleList;
+    private HashSet<Path> changedCompileList;
     private String projectName;
 
     public Project(File file) {
         super(file);
+        this.moduleList = new ArrayList<>();
+        this.changedCompileList = new HashSet<>();
     }
 
     public List<Module> getModuleList() {
@@ -29,5 +32,13 @@ public class Project extends Module {
 
     public void setProjectName(String projectName) {
         this.projectName = projectName;
+    }
+
+    public HashSet<Path> getChangedCompileList() {
+        return changedCompileList;
+    }
+
+    public void setChangedCompileList(HashSet<Path> changedCompileList) {
+        this.changedCompileList = changedCompileList;
     }
 }
